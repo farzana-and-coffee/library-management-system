cb2a99c4b06ba54f219c073b2808ea6f4a697eda
Farzana <your@email.com>
2026-06-30 06:59:35 +0300

    addeed book class and created library class

---
+++ book.java
@@ -0,0 +1,39 @@
+public class book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isBorrowed;
+
+    public book(String title, String author, String isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn ; 
+        this.isBorrowed = false;
+
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public String getIsbn(){
+        return isbn;
+    }
+
+    public boolean isBorrowed(){
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
+
+    public void displayInfo(){
+        System.out.println(title + "by " + author + " (ISBN: "+ isbn + ") - " + (isBorrowed ? "Borrowed" : "Available") );
+    }
+
+}
